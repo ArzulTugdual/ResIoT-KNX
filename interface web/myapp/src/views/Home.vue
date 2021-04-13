63ddf6413d9c99a48cd685cd2337fea0df62f568
Tugdual <tugdualarzul@gmail.com>
2021-04-13 11:20:01 +0200

    up

--- interface web/myapp/src/views/Home.vue
+++ interface web/myapp/src/views/Home.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="home">
-    <img  v-if="rouge" src="../assets/Button_Icon_Red.svg.png" v-on:click="changeImage()" />
-    <img  v-else src="../assets/Button_Icon_Green.svg.png" v-on:click="changeImage()" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div v-if="red">
+      <img  alt="rouge" src="../assets/Button_Icon_Red.png" v-on:click="changeImage()" >
+      <p>allumer le chenillard</p>
+    </div>
+    <div v-else>
+      <img alt="vert" src="../assets/Button_Icon_Green.svg.png" v-on:click="changeImage()" >
+      <p>arrêter le chenillard</p>
+    </div>
   </div>
 </template>
 
+<script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
@@ -17,9 +23,9 @@ import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 })
 
 export default class Home extends Vue {
-  rouge=true
-  changeImage () {
-    this.rouge = !this.rouge
+  red=true;
+  changeImage() {
+    this.red = !this.red;
   }
 }
 </script>
